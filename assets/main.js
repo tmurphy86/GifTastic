820fa175a1e0fd1cc35429b62385ee5fc76b7107
Tim Murphy <tim.murphy86@gmail.com>
2017-06-27 19:17:20 -0400

    transfer to work computer 2

--- assets/main.js
+++ assets/main.js
@@ -60,10 +60,25 @@ function search(){
 
 }
 
+function animate() {
+      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+      var state = $(this).attr("data-state");
+      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+      // Then, set the image's data-state to animate
+      // Else set src to the data-still value
+      if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+      } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+      }
+};
+
 function topicAdd() {
   for (var i = 0; i < topics.length; i++) {
-     $('#topics').append($('<button class='button btn btn-primary' >'));
-
+     $('#topics').append($('<button>', {class:'button btn btn-primary', "data-person", 'topics[i]'}));
+     
       // In this case, the "this" keyword refers to the button that was clicked
       person = $(this).attr("data-person");
 
@@ -74,4 +89,5 @@ function topicAdd() {
 
 $(document).on('click', '.button', search);
 
+$(document).on('click', '.gif', animate);
 });
