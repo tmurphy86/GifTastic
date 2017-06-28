fe4678e3d4ba326167987dce8be979bb0d3f8de4
unknown <tmurphy@CFED-DC-2500.headquarters.cfed.org>
2017-06-27 21:31:53 -0400

    done minus UI

--- assets/main.js
+++ assets/main.js
@@ -1,22 +1,42 @@
 $(function(){ 
-    var topics = [];
+    var topics = ['Terrifying', 'Sad', 'Happy', 'Excited', 'Loved', 'Suprised'];
     var topic = '';
     var person;
 
-    // $('#drop').html('<div class="form-control" id="start-input" type="text">');
+    topicAdd();
 
-    $('#submit').on('click', function() {
-      topic = $('#input').val().trim();
-      topics.push(topic);
-      search();
-      topicAdd();
-      //run function to do 
 
-    });
-    
-function search(){
+  $('#submit').on('click', function() {
+    topic = $('#input').val().trim();
+    $('#input').val("");
+    topics.push(topic);
+    topicAdd();
+    person = topic;
+    search();
+   });
 
-      // Constructing a URL to search Giphy for the name of the person who said the quote
+
+  $('.gifPic').on('click', function() {
+    person = $(this).attr("data-person");
+    animate();
+   });
+
+
+  $('.giftopic').on('click', function() {
+    clearGif();
+    person = $(this).attr("data-person");
+    search();
+   });
+
+
+  function clearGif(){
+   $(".item").remove();
+
+  }
+
+  function search(){
+
+      // Constructing a URL to search Giphy for the value in the text
       var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         person + "&api_key=dc6zaTOxFJmzC&limit=10";
 
@@ -36,18 +56,11 @@ function search(){
               // Creating a div with the class "item"
               var gifDiv = $("<div class='item'>");
 
-              // Storing the result item's rating
-              var rating = results[i].rating;
-
               // Creating a paragraph tag with the result item's rating
-              var p = $("<p>").text("Rating: " + rating);
+              var p = $("<p>").text("Rating: " + results[i].rating);
 
               // Creating an image tag
-              var personImage = $("<img>");
-
-              // Giving the image tag an src attribute of a proprty pulled off the
-              // result item
-              personImage.attr("src", results[i].images.fixed_height.url, "data-status", "running");
+              var personImage = $("<img>", {src: results[i].images.fixed_height_still.url, class:"gifPic", "data-animate": results[i].images.fixed_height.url, "data-still": results[i].images.fixed_height_still.url, "data-state": "still"});
 
               // Appending the paragraph and personImage we created to the "gifDiv" div we created
               gifDiv.append(p);
@@ -58,36 +71,36 @@ function search(){
           }
         });
 
-}
+  }
 
-function animate() {
-      // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+  function animate() {
+     
       var state = $(this).attr("data-state");
-      // If the clicked image's state is still, update its src attribute to what its data-animate value is.
-      // Then, set the image's data-state to animate
-      // Else set src to the data-still value
+     
       if (state === "still") {
         $(this).attr("src", $(this).attr("data-animate"));
         $(this).attr("data-state", "animate");
       } else {
+
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
+
       }
-};
+  };
 
-function topicAdd() {
-  for (var i = 0; i < topics.length; i++) {
-     $('#topics').append($('<button>', {class:'button btn btn-primary', "data-person", 'topics[i]'}));
-     
-      // In this case, the "this" keyword refers to the button that was clicked
-      person = $(this).attr("data-person");
+  function topicAdd() {
+    $(".giftopic").remove();
+    clearGif();
+
+    for (var i = 0; i < topics.length; i++) {
+        $('#topics').append($('<button>', {class:'button btn btn-primary giftopic', "data-person": topics[i], text: topics[i]}));
+    }
+  };
 
 
-  }
 
-};
 
-$(document).on('click', '.button', search);
+$(document).on('click', '.giftopic', search);
 
-$(document).on('click', '.gif', animate);
+$(document).on('click', '.gifPic', animate);
 });
